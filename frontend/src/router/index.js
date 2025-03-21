2877b8f91abd5fdcf7e8f9e789bb72662756a9e5
migi-gluttony <137354823+migi-gluttony@users.noreply.github.com>
2025-03-21 17:03:58 +0000

    A commit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,35 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import ResetPasswordView from '../views/ResetPasswordView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/home',
-      name: 'home',
-      component: HomeView,
-      meta: { guest: true }
-
-    },
-    {
       path: '/login',
       name: 'login',
       component: LoginView,
-      meta: { guest: true }
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView,
-      meta: { guest: true }
-    },
-    {
-      path: '/reset-password',
-      name: 'resetPassword',
-      component: ResetPasswordView,
       meta: { guest: true }
     }
   ]
